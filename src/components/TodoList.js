3a862fd81b972281da2a993639cf1a49620d26be
surelinks <fumsamuel@gmail.com>
2020-11-02 14:09:22 +0100

    To do list first init

---
+++ src/components/TodoList.js
@@ -0,0 +1,20 @@
+import React from "react";
+// Import components 
+import Todo from "./Todo"
+const TodoList =({todos, setTodos, filteredTodos}) =>{
+
+    return (
+        <div class="todo-container">
+            <ul class="todo-list">
+                {filteredTodos.map(todo => (
+                    <Todo setTodos={setTodos} 
+                    todo={todo}
+                    todos={todos} key={todo.id} text={todo.text} />
+                ))}
+            </ul>
+        </div>
+    )
+};
+
+export default TodoList;
+
